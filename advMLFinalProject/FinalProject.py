f982b0fcb7c3cac5753f8daf2319c346f042c698
ああああああああ\Yayoi <no>
2018-12-14 20:05:52 -0800

    updates

--- advMLFinalProject/FinalProject.py
+++ advMLFinalProject/FinalProject.py
@@ -26,11 +26,12 @@ y_modified = np.reshape(y, (len(y), seq_length))
 # @params: X: the input array to fetch a batch from
 # @params: y the output array to fetch a corosponding batch from
 # @params: batch_size: The size of the batch you want to pull
-def fetchBatch(X,y,batch_size):
-    idx = np.random.randint((len(X)-batch_size)-1)
-    X_batch = X[idx:idx+batch_size,:]
-    y_batch = X[idx:idx+batch_size,:]
-    yield X_batch, y_batch
+def fetchBatch(X,y,batch_size, num_batches):
+    for i in range(num_batches):
+        idx = np.random.randint((len(X)-batch_size)-1)
+        X_batch = X[idx:idx+batch_size]
+        y_batch = y[idx:idx+batch_size]
+        yield X_batch, y_batch
 
 # Pulls a single random row from X_modifed
 def single_batch(X):
@@ -47,37 +48,31 @@ n_outputs = vocab_size
 n_steps = 25
 n_neurons = 100
 
-# FIXME: add tf names to seperate this graph from others we make 
-
-
 X = tf.placeholder(tf.int32, [None, n_steps])
-# in line one hot encoding in tf, we tried to one hot everything then pass it through but had trouble creating a fetch batch for it
-one_hot_X = tf.one_hot(X, vocab_size)
+one_hot_X = tf.one_hot(X, vocab_size, dtype=tf.float32)
 y = tf.placeholder(tf.int32, [None, n_steps])
 
-cell = tf.contrib.rnn.OutputProjectionWrapper(tf.contrib.rnn.BasicRNNCell(
-        num_units=n_neurons, activation=tf.nn.relu),output_size=n_outputs)
-    
-outputs, states = tf.nn.dynamic_rnn(cell, one_hot_X, dtype=tf.float32) # this takes size [None, n_steps, n_inputs]
-learning_rate = 0.001
+layers = tf.contrib.rnn.OutputProjectionWrapper(
+         tf.contrib.rnn.BasicRNNCell(num_units=n_neurons),
+         output_size=vocab_size, activation=tf.nn.tanh)
 
-y_probs = tf.nn.softmax(logits=outputs)
-xenthropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=outputs, labels=y)
+
+brnn_layers = [tf.contrib.rnn.BasicRNNCell(size) for size in [n_neurons,250]]
+cell = tf.contrib.rnn.OutputProjectionWrapper(tf.contrib.rnn.MultiRNNCell(brnn_layers),output_size=vocab_size, activation=tf.nn.tanh)
+output, states = tf.nn.dynamic_rnn(cell, one_hot_X, dtype=tf.float32) # this takes size [None, n_steps, n_inputs]
+learning_rate = 0.01
+
+y_probs = tf.nn.softmax(output)
+xenthropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=output, labels=y)
 loss = tf.reduce_mean(xenthropy)
 optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
 training_op = optimizer.minimize(loss)
-
-
-
-
-# sample is outputing repeating characters, why?
 
 def sample(seed_pred, n):
     start_idx = 0
     # list of indexes we predict
     output = []
     # Debug for seeing the prob distributions
-    probs_list = []
     
     for i in range(n):
       # This creates the moving window that starts with our input sequence, and the new input is appended at each loop
@@ -86,7 +81,6 @@ def sample(seed_pred, n):
       window = window.reshape((1, n_steps))
       # We evaluate the softmax of the outputs by inserting our array window into the network and getting the logits
       char_prob = y_probs.eval(feed_dict={X:window})
-      probs_list.append(char_prob)
       # We only care about the last character softmaxed outputs, as this is the probability of a choice of one of len(vocab_size) characters
       last_char = char_prob[0][n_steps-1]
       # We get an index value by pulling a random value /according to the probabilty distribution of the last_chars softmax      
@@ -99,31 +93,31 @@ def sample(seed_pred, n):
       seed_pred = np.append(seed_pred, ind, axis=0)
       # move the window up one and continue
       start_idx +=1
-    return output, probs_list
+    return output
+
+
+
 
 
 
 # FIXME add accuracy, refer to Geron pg 397
 init = tf.global_variables_initializer()
 
-batch_list = []
-n_epochs = 100000
-batch_size = 50
+n_epochs = 100
+batch_size = 25
+num_batches = 50
 with tf.Session() as sess:
     init.run()
     for epoch in range(n_epochs):
-        for X_batch, y_batch in fetchBatch(X_modified,y_modified,batch_size):
+        for X_batch, y_batch in fetchBatch(X_modified,y_modified,batch_size,50):
             sess.run(training_op, feed_dict={X:X_batch, y:y_batch})
-            if epoch % 10000 == 0:
-                cur_loss = loss.eval(feed_dict={X:X_batch, y:y_batch})
-                print(f"Loss: \t{cur_loss}")
-                solo_batch = single_batch(X_modified)
-                batch_list.append(solo_batch)
-                txt = ''.join(ix_to_char[ix] for ix in solo_batch)
-                print('----SOLO BATCH \n %s \n----' % (txt, ))
-                smpl, _ = sample(solo_batch,100)
-                txt = ''.join(ix_to_char[ix] for ix in smpl)
-                print('----OUTPUT \n %s \n----' % (txt, ))
+        if epoch % 10 == 0:
+            cur_loss = loss.eval(feed_dict={X:X_batch, y:y_batch})
+            print(f"Loss: \t{cur_loss}")
+            solo_batch = single_batch(X_modified)
+            smpl = sample(solo_batch,25)
+            txt = ''.join(ix_to_char[ix] for ix in smpl)
+            print('----OUTPUT \n %s \n----' % (txt, ))
                 
                     
 
